945843a03effefeec70a9d9d9f7f2f5e28cc2e74
jose-perez16 <jose.perez138@upr.edu>
2026-05-05 19:16:22 -0400

    Implementacion del horario (7am - 5pm) y el minimo/maximo de horas (1-8hrs)

--- src/model/ValidReserva.java
+++ src/model/ValidReserva.java
@@ -1,5 +1,24 @@
 package model;
 
 public class ValidReserva {
-    
+    public static void validarHorario(int horaInicial, int horaFinal) throws HorarioException{
+        if(horaInicial < 7 || horaInicial > 16){
+            throw new HorarioException("La hora inicial debe estra entre 7am y 16pm (4pm)");
+        }
+        if(horaFinal < 8 || horaFinal > 17){
+            throw new HorarioException("La hora final debe estar entre 8am y 17pm (5pm)");
+        }
+        if(horaFinal <= horaInicial){
+            throw new HorarioException("La hora final debe ser mayor que la hora de inicio.");
+        }
+        int tiempo = horaFinal - horaInicial;
+
+        if(tiempo < 1){
+            throw new HorarioException("Debes reservar un minimo de 1 hora");
+        }
+
+        if(tiempo > 8){
+            throw new HorarioException("El maximo que puede durar una reservacion es 8 horas.");
+        }
+    }
 }
