127a2d79666ca3c6a9665b855d8bcd9ee7c33b6b
jose-perez16 <jose.perez138@upr.edu>
2026-05-05 15:41:33 -0400

    Implementacion generacion de IDs para reservaciones, grupos y transacciones.

--- src/model/IdGen.java
+++ src/model/IdGen.java
@@ -1,5 +1,19 @@
 package model;
 
 public class IdGen {
-    
+    private static int contadorR = 1;
+    private static int contadorG = 1;
+    private static int contadorT = 1;
+
+    public static String nuevoIdReservacion(){
+        return String.format("R-%03d", contadorR++);
+    }
+
+    public static String nuevoIdGrupo(){
+        return String.format("G-%03d", contadorG++);
+    }
+
+    public static String nuevoIdTransaccion(){
+        return String.format("T-%03d", contadorT++);
+    }
 }
