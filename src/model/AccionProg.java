42571c2ed5d997997c9c23f50e92fc3d5f5300fd
jose-perez16 <jose.perez138@upr.edu>
2026-05-05 20:07:39 -0400

    Implementacion de AccionProg para el UNDO.

--- src/model/AccionProg.java
+++ src/model/AccionProg.java
@@ -1,5 +1,42 @@
 package model;
 
 public class AccionProg {
-    
+    public enum Tipo{ RESERVAR, CANCELAR, CAMBIAR}
+
+    private Tipo tipo;
+    private Reservacion reservacion;
+    private Espacio espacioAnterior;
+
+    public AccionProg(Tipo tipo, Reservacion reservacion, Espacio espacioAnterior){
+        this.tipo = tipo;
+        this.reservacion = reservacion;
+        this.espacioAnterior = espacioAnterior;
+    }
+
+    public Tipo geTipo(){
+        return tipo;
+    }
+
+    public Reservacion getReservacion(){
+        return reservacion;
+    }
+
+    public Espacio getEspacioAnterior(){
+        return espacioAnterior;
+    }
+
+    @Override
+    public String toString(){
+        String espacioStr;
+
+        if(espacioAnterior != null){
+            espacioStr = "Espacio #" + espacioAnterior.getNumeroEspacio();
+        }else{
+            espacioStr = "(ninguno)";
+        }
+
+        return "Accion: " + tipo +
+                "\n Reservacion: " + reservacion.getIdReservacion() +
+                "\nEspacio anterior: " + espacioStr;
+    }
 }
