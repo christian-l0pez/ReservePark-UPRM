4db13725f5cd5cfb8486bab0376a50ab0f447d53
jose-perez16 <jose.perez138@upr.edu>
2026-05-07 17:17:58 -0400

    Documentacion/commentarios del codigo

--- src/model/MenuOp.java
+++ src/model/MenuOp.java
@@ -1,10 +1,23 @@
 package model;
 import java.util.*;
+
+// Clase principal que maneja la interfaz de usuario y las operaciones del sistema de reservacion de estacionamiento.
+// Lee input del usuario, usa metodos de Estacionamiento y mustra resultados.
+
+// Estructuras de datos usadas:
+// Estacionamiento: contiene todas las reservaciones, transacciones, lista de espera, etc.
+// ArrayList<ServicioAdiconal> para construir la lista de opciones disponibles y la lista de servicios escogidos.
+// Set<Espacio> para determinar el espacio asignado.
+// Queue<Reservacion> muestra la lista de espera.
+// LinkedList<Transaccion> muestra el historial de transacciones.
 public class MenuOp {
     public static final Scanner scanner = new Scanner(System.in);
     public static final Estacionamiento estacionamiento = new Estacionamiento();
     
-    public static void main(String[] args){
+    // main para poder correr el programa desde MenuOp. Imprime el inicio del programa, luego por un loop mustra el menu,
+    // procesa la opcion del usuario y cualquier excepcion se captura y se imprime.
+    // Llama a Menu() y todos los metodos del menu (hacerReservaciones(),cancelar(), cambiarSeccion(), etc.)
+    public static void main(String[] args){ 
         System.out.println("+===================================================+");
         System.out.println("  Sistema de Reservacion de Estacionamiento - UPRM");
         System.out.println("+===================================================+");
@@ -41,6 +54,10 @@ public class MenuOp {
         System.out.println("Proceso finalizado.");
     }
 
+    // Menu - imprime el menu con las opciones que puedes escoger.
+    // No recibe ni devuelve valores
+    // Llama a los prints.
+    // Es llamado por main()
     private static void Menu(){
         System.out.println("========= Menu =========");
         System.out.println("1. Hacer reservacion");
@@ -59,6 +76,11 @@ public class MenuOp {
         System.out.print("Escoja una opcion: ");
     }
 
+    // Le pide al usuario datos del estudiante y crea un objeto
+    // Recibe user input(nombre,numero,email,telefono).
+    // Devuelve el estudiante con la informacion recibida
+    // Llama al scanner y new Estudiante()
+    // Llamado por hacerReservacion()
     private static Estudiante infoEstudiante(){
         System.out.print("Nombre del Estudiante: ");
         String nombre = scanner.nextLine().trim();
@@ -75,6 +97,11 @@ public class MenuOp {
         return new Estudiante(numero, email, telefono, nombre);
     }
 
+    // Como en infoEstuddiante() le pide al usuario informacion del auto y crea un objeto.
+    // Recibe user input(marca,tablilla,model, year)
+    // Devuelve el Auto con la informacion recibida
+    // Llama al scanner y new Auto()
+    // Llamado por hacerReservacion()
     private static Auto infoAuto(){
         System.out.print("Marca del Auto: ");
         String marca = scanner.nextLine().trim();
@@ -92,6 +119,12 @@ public class MenuOp {
         return new Auto(marca, tablilla, modelo, year);
     }
 
+    // Pide al usuario que escoja una seccion.
+    // tira un exception si el numero no es 1, 2 o 3 ya que esos son las opciones correspondientes a las secciones
+    // Recibe user input (1,2,3) para elegir una seccion
+    // Devuelve TipoSeccion y la seccion
+    // Llama al scanner
+    // Llamado por hacerReservacion(), cambiarSeccion(), listaDeEspera() y espacioDisponible()
     private static TipoSeccion seccion(){
         System.out.println("Seccion: 1. GENERAL($2/hr) , 2. VIP(4/hr), 3. ELECTRICO($8/hr)");
         System.out.print("Elige una seccion: ");
@@ -104,6 +137,12 @@ public class MenuOp {
         }
     }
 
+    // Pide al usuario que escoja un dia de la semana para la reservacion
+    // tira un exception si el numero no es (1-5) ya que estos corresponden a los dias de la semana.
+    // Recibe user input (1,2,3,4,5) para escojer un dia
+    // Devuelve DiaSemana y el dia
+    // Llama al scanner
+    // Llamado por hacerReservacion(), mayoresDe2(), porVentana() y espaciosDisponibles()
     private static DiaSemana dia(){
         System.out.print("Dia (1=Lunes, 2= Martes, 3=Miercoles, 4=Jueves, 5= Viernes): ");
         int numero = scanner.nextInt(); scanner.nextLine();
@@ -118,6 +157,13 @@ public class MenuOp {
         }
     }
 
+    // Construye una lista con los servicios disponibles sengun la seccion para seleccionar servicios.
+    // Se puede escoger mas de 1 ocpion hasta que se escoja "0) terminar"
+    // El ArrayList se usa para los disponibles y otro para las opciones escogidas. Tamiben se usa un * para marcar los escogidos.
+    // Recibe seccion(TipoSeccion) y user input 
+    // Devuleve ArrayList<ServicioAdicional> con los servicios escogiods por el usuario.
+    // Llama a new ServicioAdicional(), scanner, escogidos.contains(), escogidos.add(), serviciosDisponibles.get(), seriviciosDisponibles.size()
+    // Llamado por hacerReservacion()
     private static ArrayList<ServicioAdicional> serviciosAdicionales(TipoSeccion seccion) {
     ArrayList<ServicioAdicional> serviciosDisponibles = new ArrayList<>();
 
@@ -182,6 +228,11 @@ public class MenuOp {
         return escogidos;
     }
 
+    // Se usa para imprimir una lista de reservaciones
+    // Si la misma esta vacia o null imprime un mensaje instead.
+    // Recibe lista(ArrayList<Reservacion>)
+    // Llama: lista.isEmtpy(), reservacion.toString(),
+    // Llamado por: semana(), mayoresDe2(), porCosto(), porVentana(), porEstudiante()
     private static void imprimirReserv(ArrayList<Reservacion> lista){
         if(lista == null || lista.isEmpty()){
             System.out.println("No se encontraron reservaciones.");
@@ -193,6 +244,11 @@ public class MenuOp {
         }
     }
 
+    // Crea la reservacion del parking, verifica los espacios libres, las horas disponibles etc. Da auto-assign un parking despues que se crea la reservacion
+    // Recibe info del estudiante, auto, seccion, dia, horas
+    // Llama a infoEstudiante(), infoAuto(), seccion(), dia(), scanner, estacionamiento.obtenerEspaciosDisponibles(), estacionamiento.agregarAlistaEspera(),
+    // estacionamient.getColaEspera(), estacionamiento.crearReservacion(), CalcCosto.calcularCostoBase(), serviciosAdicionales(), new Espacio(), new Reservacion(), IdGen.nuevoIdReservacion()
+    // LLamado por main() {el caso 1}
     private static void hacerReservacion() throws Exception{
         System.out.println("=== Nueva Reservacion ===");
         Estudiante estudiante = infoEstudiante();
@@ -243,6 +299,10 @@ public class MenuOp {
         System.out.println(reserv);
     }
 
+    // Pide el ID de la reservacion para cancelarla. Tambien muestra cargo de cancelacion
+    // Recibe el ID de la reservacion a traves de user input
+    // Llama al scanner, estacionamiento.cancelarReservacion(), calcCosto.calcularCostoCancelacion()
+    // Llamado por main (opcion 2)
     private static void cancelar() throws Exception{
         System.out.println("=== Cancelar Reservacion ===");
         System.out.print("ID de la reservacion: ");
@@ -252,6 +312,11 @@ public class MenuOp {
         System.out.println("Cargo por cancelar: $" + CalcCosto.calcularCostoCancelacion());
     }
 
+    // Pide el ID de reservacion para buscar un neuevo espacio en otra seccion para el mismo dia/hora. Bloquea si la seccion nueva es igual a la actual
+    // Recibe ID de la reservacion a traves de user input
+    // Llama scanner, estacionamiento.buscarReservacionPorIdd(), estacionamiento.obternerEspaciosDisponibles(), estacionamiento.cambiarEspacio(), seccion()
+    // reservacion.getEspacio(), reservacion.getDia(), reservacion.getHoraINicio(), reservacion.getHoraFin(), espacio.getSeccion(), espacio.getNumeroEspacio()
+    // Llamado por main (opcion 3)
     private static void cambiarSeccion() throws Exception{
         System.out.println("=== Cambiar Reservacion de Seccion ===");
         System.out.print("ID de la reservacion: ");
@@ -287,17 +352,28 @@ public class MenuOp {
         System.out.println(cambio);
     }
 
+    // Deshace la ultima accion realizada
+    // Llama a estacionamiento.getDeshacerUltimaAccion()
+    // Llamado por main() (opcion 4)
     private static void undo(){
         System.out.println("=== UNDO ultima accion ===");
         estacionamiento.deshacerUltimaAccion();
     }
 
+    // Muestra toda las reservaciones en la semana
+    // recibe las reservaciones
+    // Llama a estacionamiento.obtenerTodasLasReservaciones(), imprimirReserv()
+    // Llamado por main() (opcion 5)
     private static void semana(){
         System.out.println("=== Reservacion de la semana ===");
         ArrayList<Reservacion> reservaciones = estacionamiento.obtenerTodasLasReservaciones();
         imprimirReserv(reservaciones); 
     }
 
+    // Muestra las reservaciones que duran mas de 2 horas
+    // recibe informacion de reservacion
+    // Llama dia(), estacionamiento.obtenerReservacionesMayorDosHoras(), imprimirReserv()
+    // Llamado por main (opcion 5)
     private static void mayoresDe2(){
         System.out.println("=== Reservacion Mayores de 2 Horas ===");
         DiaSemana dia = dia();
@@ -305,6 +381,10 @@ public class MenuOp {
         imprimirReserv(resultado);
     }
 
+    // Mustra las reservaciones que caen dentro de un rango de $ 
+    // Recibe user input (1, 2 o 3) 
+    // Llama scanner, estacionamiento.obtenerReservacionesPorRangoCosto(), imprimirReserv()
+    // Llamado por main() (opcion 7)
     private static void porCosto(){
         System.out.println("=== Reservacion por costo ===");
         System.out.println("1. Mayor a $50");
@@ -338,6 +418,10 @@ public class MenuOp {
 
     }
 
+    // Mustra las reservaciones que caen dentro de una ventana de tiempo 
+    // Recibe informacion del dia, user input para determinar la hora inicial y la hora finaal 
+    // Llama dia(), scanner, estacionamiento.obtenerReservacionesPorPeriodo(), imprimirReserv()
+    // Llamado por main() (opcion 8)
     private static void porVentana(){
         System.out.println("=== Reservacion en Ventana de Tiempo ===");
         DiaSemana dia = dia();
@@ -351,6 +435,10 @@ public class MenuOp {
         imprimirReserv(completado);
     }
 
+    // Muestra las reservaciones de un estudiante segun su numero de estudiante
+    // recibe user input (numero de estudiante)
+    // Llama scanner, estacionamiento.buscarReservacionesPorEstudiante() imprimirReserv()
+    // Llamado por main() (opcion 9)
     private static void porEstudiante(){
         System.out.println("=== Reservacion por Estudiante ===");
         System.out.print("Numero de estudiante: ");
@@ -360,6 +448,9 @@ public class MenuOp {
         imprimirReserv(resultado);
     }
 
+    // Muestra todas las transacciones (reservaciones,cambioes,cancelaciones,cargos)
+    // LLama a estacionamiento.obtenerTransacciones(), transacciones.isEmpty().
+    // Llamado por main() (opcion 10)
     private static void transacciones(){
         System.out.println("=== Historial de transacciones ===");
         LinkedList<Transaccion> transacciones = estacionamiento.obtenerTransacciones();
@@ -373,6 +464,9 @@ public class MenuOp {
         }
     }
 
+    // Muestra la lista de espera por seccion escogida, tambien indica la posicion.
+    // Llama seccion(), estacionamiento.getColaEspera(), cola.isEmpty()
+    // Llamado por main() (opcion 11)
     private static void listaDeEspera(){
         System.out.println("=== Lista de espera ===");
         TipoSeccion secEscogida = seccion();
@@ -391,6 +485,11 @@ public class MenuOp {
             pos++;
         }
     }
+
+    // Muestra cuantos espacios disponibles hay por seccion para un dia / rango de horario
+    // Recibe user input hora inicial, hora final
+    // Llama seccion(), dia(), scanner, estacionamiento.ObtenerEspaciosDisponibles(), espaciosLibres.size()
+    // Llamado por main() (opcion 12)
     private static void espaciosDisponibles() throws Exception{
         System.out.println("=== Espacios disponibles === ");
         TipoSeccion secEscogida = seccion();
